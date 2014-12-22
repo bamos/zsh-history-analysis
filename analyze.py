79e34870e154cf4b7cbe26c044a90c97f366826c
Brandon Amos <bamos@cmu.edu>
2014-12-21 23:55:16 -0500

    Add hour and weekday breakdowns with means and stdevs. Add top commands.

--- analyze.py
+++ analyze.py
@@ -4,6 +4,15 @@ import argparse
 import os
 import sys
 import time
+from itertools import groupby
+from collections import Counter,defaultdict
+import statistics
+
+def groupByKey(m):
+    groupedM = defaultdict(list)
+    for k,v in m:
+        groupedM[k].append(v)
+    return groupedM
 
 class Command:
     def __init__(self,raw):
@@ -14,27 +23,68 @@ class Command:
       self.base_command = tup[1].split()[0]
 
 class HistoryData:
-    def __init__(self,filename):
-        with open(filename,'rb') as f:
-            commands = []
-            it = iter(f)
-            for line in it:
-                try:
-                    full_line = line.decode()
-                    while full_line.strip()[-1] == '\\':
-                        full_line += next(it).decode()
-                    commands.append(Command(full_line))
-                except Exception as e:
-                    #print("Warning: Exception parsing.") # TODO
-                    #print(e)
-                    pass
-            self.commands=commands
+    def __init__(self,filenames):
+        if isinstance(filenames,str):
+            filenames = [filenames]
+        commands = []
+        for filename in filenames:
+            with open(filename,'rb') as f:
+                it = iter(f)
+                for line in it:
+                    try:
+                        full_line = line.decode()
+                        while full_line.strip()[-1] == '\\':
+                            full_line += next(it).decode()
+                        commands.append(Command(full_line))
+                    except Exception as e:
+                        #print("Warning: Exception parsing.") # TODO
+                        #print(e)
+                        pass
+        self.commands=commands
 
-    def get_times_hour(self):
-        return [cmd.timestamp_struct.tm_hour for cmd in self.commands]
+    def get_hourly_breakdowns(self):
+        days = self.group_by_day()
+        all_freqs = [[] for x in range(24)]
+        for day,cmds in sorted(days.items()):
+            day_times = [cmd.timestamp_struct.tm_hour for cmd in cmds]
+            freq_counter = Counter(day_times)
+            freqs = [0 for x in range(24)]
+            for hour,num in freq_counter.items():
+                freqs[hour] = num
+            for hour,num in enumerate(freqs):
+                all_freqs[hour].append(num)
+        means = []; stdevs = []
+        for hour_freqs in all_freqs:
+            means.append(statistics.mean(hour_freqs))
+            stdevs.append(statistics.stdev(hour_freqs))
+        return means,stdevs
 
-    def get_times_wday(self):
-        return [cmd.timestamp_struct.tm_wday for cmd in self.commands]
+    def get_weekday_breakdowns(self):
+        days = self.group_by_day()
+        all_freqs = [[] for x in range(7)]
+        for day,cmds in sorted(days.items()):
+            weekdays = [cmd.timestamp_struct.tm_wday for cmd in cmds]
+            freq_counter = Counter(weekdays)
+            freqs = [0 for x in range(7)]
+            for day,num in freq_counter.items():
+                freqs[day] = num
+            for day,num in enumerate(freqs):
+                all_freqs[day].append(num)
+        means = []; stdevs = []
+        for day_freqs in all_freqs:
+            means.append(statistics.mean(day_freqs))
+            stdevs.append(statistics.stdev(day_freqs))
+        return means,stdevs
+
+    def group_by_day(self):
+        ts = [(cmd.timestamp_struct,cmd) for cmd in self.commands]
+        kv = groupByKey(
+            [("{}-{}-{}".format(t.tm_year,t.tm_mon,t.tm_mday),cmd)
+             for t,cmd in ts])
+        return kv
+
+    def get_base_commands(self):
+        return [cmd.base_command for cmd in self.commands]
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
@@ -45,6 +95,8 @@ if __name__=='__main__':
     subparsers = parser.add_subparsers(help='sub-command help',dest='cmd')
     subparsers.required = True
     parser_timeFrequencies = subparsers.add_parser('timeFrequencies')
+    parser_topCommands = subparsers.add_parser('topCommands')
+    parser_topCommands.add_argument("--num",type=int,default=15)
 
     args = parser.parse_args()
 
@@ -54,19 +106,27 @@ if __name__=='__main__':
     mkdir_p(args.analysis_dir)
     mkdir_p(args.plots_dir)
 
-    all_history = []
-    for filename in os.listdir(args.history_dir):
-        all_history.append(HistoryData(args.history_dir+"/"+filename))
+    hist_files = [args.history_dir+"/"+x for x in os.listdir(args.history_dir)]
+    all_hist = HistoryData(hist_files)
 
     if args.cmd == 'timeFrequencies':
-        hours = []
-        for history in all_history:
-            hours += history.get_times_hour()
+        hour_means, hour_stdevs = all_hist.get_hourly_breakdowns()
         with open(args.analysis_dir+"/time-hours.csv","w") as f:
-            f.write(",".join([str(h) for h in hours])+"\n")
+            f.write(",".join([str(h) for h in hour_means])+"\n")
+            f.write(",".join([str(h) for h in hour_stdevs])+"\n")
 
-        wdays = []
-        for history in all_history:
-            wdays += history.get_times_wday()
+        wday_means, wday_stdevs = all_hist.get_weekday_breakdowns()
         with open(args.analysis_dir+"/time-wdays.csv","w") as f:
-            f.write(",".join([str(wd) for wd in wdays])+"\n")
+            f.write(",".join([str(h) for h in wday_means])+"\n")
+            f.write(",".join([str(h) for h in wday_stdevs])+"\n")
+    elif args.cmd == 'topCommands':
+        cmds = []
+        for history in all_history:
+            cmds += history.get_base_commands()
+        with open(args.analysis_dir+"/top-cmds.csv","w") as f:
+            print("Frequency | Command")
+            print("---|---")
+            f.write("{},{}\n".format("Frequency","Command"))
+            for tup in Counter(cmds).most_common(args.num):
+                print("{} | {}".format(tup[1],tup[0]))
+                f.write("{},{}\n".format(tup[1],tup[0]))
--- plot.r
+++ plot.r
@@ -6,20 +6,37 @@
 library(ggplot2)
 library(reshape)
 
-hours = as.numeric(read.csv("analysis/time-hours.csv",header=F))
+hours_m = as.numeric(read.csv("analysis/time-hours.csv",header=F,nrows=1))
+hours_stdev = as.numeric(read.csv("analysis/time-hours.csv",header=F,nrows=1,skip=1))
 
 p <- ggplot() +
   xlab("Hour of Day") +
-  ylab("Total Commands Executed") +
-  geom_histogram(aes(x=hours),binwidth=1) +
+  ylab("Average Commands Executed") +
+  geom_bar(stat='identity',aes(x=0:23.,y=hours_m)) +
   theme_bw()
-ggsave("plots/time-hours.pdf",width=7,height=6)
+  # TODO: Error bars.
+ggsave("plots/time-hours.png",width=7,height=6)
 
-
-wdays = as.numeric(read.csv("analysis/time-wdays.csv",header=F))
-p <- ggplot() +
+wdays_m = as.numeric(read.csv("analysis/time-wdays.csv",header=F,nrows=1))
+wdays_stdev = as.numeric(read.csv("analysis/time-wdays.csv",header=F,nrows=1,skip=1))
+wday_str = c("Mon","Tues","Weds","Thurs","Fri","Sat","Sun")
+df <- data.frame(freqs = wdays_m, wdays = factor(wday_str,levels=wday_str))
+p <- ggplot(df,aes(x=wdays,y=freqs)) +
   xlab("Week Day") +
-  ylab("Total Commands Executed") +
-  geom_histogram(aes(x=wdays),binwidth=1) +
+  ylab("Average Commands Executed") +
+  geom_bar(stat='identity',aes(y=freqs)) +
   theme_bw()
-ggsave("plots/time-wdays.pdf",width=7,height=6)
+  # TODO: Error bars.
+ggsave("plots/time-wdays.png",width=7,height=6)
+
+top_cmds = read.csv("analysis/top-cmds.csv",header=T)
+df <- data.frame(
+  freqs = top_cmds[[1]],
+  cmd_names = factor(top_cmds[[2]],levels=top_cmds[[2]])
+)
+p <- ggplot(df,aes(x=cmd_names,y=freqs)) +
+  xlab("Command") +
+  ylab("Frequency") +
+  geom_bar(stat='identity',aes(y=freqs)) +
+  theme_bw()
+ggsave("plots/top-cmds.png",width=7,height=6)
